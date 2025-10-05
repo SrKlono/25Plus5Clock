a037e0cbe9d77bdca20501fe7f5e03690716393c
SrKlono <danilogamasilvanb@gmail.com>
2025-10-05 08:30:06 -0300

    Passing all FCC tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import LightRays from "./components/LightRays";
 import {
 	ArrowLeftCircleIcon,
@@ -7,11 +7,65 @@ import {
 	PlayIcon,
 	RewindIcon,
 } from "lucide-react";
+import { useTimer } from "react-timer-hook";
+import { formatTime } from "./lib/utils";
 
 const App = () => {
 	const [breakLength, setBreakLength] = useState(5);
 	const [sessionLength, setSessionLength] = useState(25);
-	const [paused, setPaused] = useState(false);
+	const [inBreak, setInBreak] = useState(false);
+
+	const getTime = () => {
+		let time = new Date();
+		time.setSeconds(time.getSeconds() + sessionLength * 60);
+		return time;
+	};
+
+	const setTime = (value) => {
+		let time = new Date();
+		time.setSeconds(time.getSeconds() + value * 60);
+		return time;
+	};
+
+	const handleTimeout = async () => {
+		document.getElementById("beep").play();
+		await new Promise((r) => setTimeout(r, 1000));
+		setInBreak(!inBreak);
+		!inBreak
+			? restart(setTime(breakLength))
+			: restart(setTime(sessionLength));
+	};
+
+	const {
+		totalSeconds,
+		milliseconds,
+		seconds,
+		minutes,
+		hours,
+		days,
+		isRunning,
+		start,
+		pause,
+		resume,
+		restart,
+	} = useTimer({
+		expiryTimestamp: getTime,
+		onExpire: handleTimeout,
+		autoStart: false,
+		interval: 20,
+	});
+
+	const reset = () => {
+		setBreakLength(5);
+		setSessionLength(25);
+		setInBreak(false);
+		let sound = document.getElementById("beep");
+		sound.pause();
+		sound.currentTime = 0;
+
+		restart(setTime(25));
+		pause();
+	};
 
 	return (
 		<div>
@@ -41,11 +95,27 @@ const App = () => {
 								Break Length
 							</h4>
 							<div className="flex justify-center items-center gap-2">
-								<button id="break-decrement" className="btn-sm">
+								<button
+									id="break-decrement"
+									className="btn-sm"
+									onClick={() =>
+										setBreakLength(
+											Math.max(breakLength - 1, 1)
+										)
+									}
+								>
 									<ArrowLeftCircleIcon className="size-7" />
 								</button>
 								<span id="break-length">{breakLength}</span>
-								<button id="break-increment" className="btn-sm">
+								<button
+									id="break-increment"
+									className="btn-sm"
+									onClick={() =>
+										setBreakLength(
+											Math.min(breakLength + 1, 60)
+										)
+									}
+								>
 									<ArrowRightCircleIcon className="size-7" />
 								</button>
 							</div>
@@ -58,11 +128,39 @@ const App = () => {
 								Session Length
 							</h4>
 							<div className="flex justify-center items-center gap-2">
-								<button id="session-decrement" className="btn-sm">
+								<button
+									id="session-decrement"
+									className="btn-sm"
+									onClick={() => {
+										let value = Math.max(
+											sessionLength - 1,
+											1
+										);
+										setSessionLength(value);
+										if (!isRunning) {
+											restart(setTime(value));
+											pause();
+										}
+									}}
+								>
 									<ArrowLeftCircleIcon className="size-7" />
 								</button>
 								<span id="session-length">{sessionLength}</span>
-								<button id="session-increment" className="btn-sm">
+								<button
+									id="session-increment"
+									className="btn-sm"
+									onClick={() => {
+										let value = Math.min(
+											sessionLength + 1,
+											60
+										);
+										setSessionLength(value);
+										if (!isRunning) {
+											restart(setTime(value));
+											pause();
+										}
+									}}
+								>
 									<ArrowRightCircleIcon className="size-7" />
 								</button>
 							</div>
@@ -74,27 +172,32 @@ const App = () => {
 							id="timer-label"
 							className="text-3xl text-white/30 font-bold"
 						>
-							Session
+							{inBreak ? (
+								<span>Break</span>
+							) : (
+								<span>Session</span>
+							)}
 						</h4>
 						<h2 id="time-left" className="text-8xl">
-							25:00
+							{formatTime(totalSeconds)}
 						</h2>
+						<audio id="beep" src="/alarm_clock.mp3" />
 					</div>
 
 					<div className="flex gap-2 justify-center">
 						<button
 							id="start_stop"
 							className="btn"
-							onClick={() => setPaused(!paused)}
+							onClick={() => (isRunning ? pause() : resume())}
 						>
-							{paused ? (
+							{isRunning ? (
 								<PlayIcon className="size-10" />
 							) : (
 								<PauseIcon className="size-10" />
 							)}
 						</button>
 
-						<button id="reset" className="btn">
+						<button id="reset" className="btn" onClick={reset}>
 							<RewindIcon className="size-10" />
 						</button>
 					</div>
